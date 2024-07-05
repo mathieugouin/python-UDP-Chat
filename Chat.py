30bfcb96d6b5f6c786ee3d723d99c100503ea630
Mathieu Gouin <mgouin@gmail.com>
2024-07-05 15:53:07 +0000

    Use automatic port.

--- Chat.py
+++ Chat.py
@@ -1,3 +1,4 @@
+"""UDP Chat client/server."""
 import socket
 import threading
 import queue
@@ -7,33 +8,34 @@ import os
 
 
 #Client Code
-def ReceiveData(sock):
+def ClientReceive(sock):
     while True:
         try:
-            data,addr = sock.recvfrom(1024)
+            data, addr = sock.recvfrom(1024)
             print(data.decode('utf-8'))
         except:
             pass
 
+
 def RunClient(serverIP):
-    host = socket.gethostbyname(socket.gethostname())
-    port = random.randint(6000,10000)
+    server = (str(serverIP), 5000)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((socket.gethostbyname(socket.gethostname()), 0))
+    host, port = s.getsockname()
     print('Client IP->'+str(host)+' Port->'+str(port))
-    server = (str(serverIP),5000)
-    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    s.bind((host,port))
 
     name = input('Please write your name here: ')
     if name == '':
         name = 'Guest'+str(random.randint(1000,9999))
         print('Your name is:'+name)
-    s.sendto(name.encode('utf-8'),server)
-    threading.Thread(target=ReceiveData,args=(s,)).start()
+    print('Type qqq to quit.')
+    s.sendto(name.encode('utf-8'), server)
+    threading.Thread(target=ClientReceive, args=(s,)).start()
     while True:
         data = input()
         if data == 'qqq':
             break
-        elif data=='':
+        if data=='':
             continue
         data = '['+name+']' + '->'+ data
         s.sendto(data.encode('utf-8'),server)
@@ -44,27 +46,27 @@ def RunClient(serverIP):
 
 
 #Server Code
-def RecvData(sock,recvPackets):
+def ServerReceive(sock, recvPackets):
     while True:
-        data,addr = sock.recvfrom(1024)
-        recvPackets.put((data,addr))
+        data, addr = sock.recvfrom(1024)
+        recvPackets.put((data, addr))
 
 def RunServer():
     host = socket.gethostbyname(socket.gethostname())
     port = 5000
     print('Server hosting on IP-> '+str(host))
-    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    s.bind((host,port))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
     clients = set()
     recvPackets = queue.Queue()
 
     print('Server Running...')
 
-    threading.Thread(target=RecvData,args=(s,recvPackets)).start()
+    threading.Thread(target=ServerReceive, args=(s, recvPackets)).start()
 
     while True:
         while not recvPackets.empty():
-            data,addr = recvPackets.get()
+            data, addr = recvPackets.get()
             if addr not in clients:
                 clients.add(addr)
                 continue
@@ -76,7 +78,7 @@ def RunServer():
             print(str(addr)+data)
             for c in clients:
                 if c!=addr:
-                    s.sendto(data.encode('utf-8'),c)
+                    s.sendto(data.encode('utf-8'), c)
     s.close()
 #Serevr Code Ends Here
 
